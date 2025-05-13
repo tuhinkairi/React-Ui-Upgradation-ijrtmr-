5c0d013db0b9a05c4eb83125239274a1d4d268a5
tuhinkairi <tuhinkairii@gmail.com>
2025-05-13 17:49:45 +0530

    tailwind config added

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF8333',         // CTAs, highlights
+        primaryHover: '#ff7519',    // CTA hover
+        text_dark: '#1A1A1A',            // Deep black text
+        primary_bg: '#f9f9f9',       // Background sections
+        divider: '#e0e0e0',         // Dividers, outlines
+        text_primary: '#333333',        // Base paragraph text
+        text_secondary: '#6B7280',           // Secondary text (gray-600)
+        text_white: '#FFFFFF',           // White (default)
+        accentBlue: '#007BFF',      // Link / hover
+        warning: '#FBBF24',         // Icons / highlights
+        notification_dark: '#1e293b',        // Footer or strong sections
+        notification: '#EBF4FF',        // Notification / info backgrounds
+      }
+    }
+  }
+};
+
+export default config;
