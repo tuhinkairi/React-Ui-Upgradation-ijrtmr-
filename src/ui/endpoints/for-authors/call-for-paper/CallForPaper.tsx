6e44b1c10395c274960e93dd4a6cbd378706d3b0
tuhinkairi <tuhinkairii@gmail.com>
2025-06-27 11:57:18 +0530

    call for paper

---
+++ src/ui/endpoints/for-authors/call-for-paper/CallForPaper.tsx
@@ -0,0 +1,13 @@
+import CommonLayout from "../../../components/layout/CommonLayout";
+import Paper from "./Paper";
+
+const CallForPapers = () => {
+  return (
+
+    <CommonLayout className="px-6 bg-white text-base">
+        <Paper/>
+    </CommonLayout>
+  );
+};
+
+export default CallForPapers;
