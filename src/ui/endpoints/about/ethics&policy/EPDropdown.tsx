fa3f3a30bafb6afee020a7b1b1d30e73cacc1131
tuhinkairi <tuhinkairii@gmail.com>
2025-07-08 03:22:18 +0530

    editorial policy update

--- src/ui/endpoints/about/ethics&policy/EPDropdown.tsx
+++ src/ui/endpoints/about/ethics&policy/EPDropdown.tsx
@@ -1,8 +1,114 @@
+import { useState, type JSX } from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import PublicationContent from "../publication-policy/PublicationContent";
+import EditorialPolicy from "./policyCards/EditorialPolicy";
 
-export default function EPDropdown() {
-  return (
-    <div>
-      {/* update after verify */}
-    </div>
-  )
+interface AccordionItem {
+  id: string;
+  title: string;
+  component: JSX.Element | string;
 }
+
+const policy:AccordionItem[] = [
+  {
+    "id": "publication-ethics",
+    "title": "Publication Ethics",
+    "component": <PublicationContent/>
+  },
+  {
+    "id": "editorial-policies",
+    "title": "Editorial Policies",
+    "component": <EditorialPolicy/>
+  },
+  {
+    "id": "author-responsibilities",
+    "title": "Author Responsibilities",
+    "component": "AuthorResponsibilities"
+  },
+  {
+    "id": "instructions-for-authors",
+    "title": "Instructions for Authors",
+    "component": "InstructionsForAuthors"
+  },
+  {
+    "id": "ethics-malpractice",
+    "title": "Publication Ethics & Malpractice Statement",
+    "component": "EthicsMalpractice"
+  },
+  {
+    "id": "article-withdrawal",
+    "title": "Article withdraw policy",
+    "component": "ArticleWithdrawPolicy"
+  },
+  {
+    "id": "plagiarism-policy",
+    "title": "Plagiarism Policy",
+    "component": "PlagiarismPolicy"
+  },
+  {
+    "id": "peer-review-policy",
+    "title": "Peer Review Policy",
+    "component": "PeerReviewPolicy"
+  },
+  {
+    "id": "copyright-ownership",
+    "title": "Copyright, Grants and Ownership Declaration",
+    "component": "CopyrightOwnership"
+  },
+  {
+    "id": "open-access-policy",
+    "title": "Open Access Policy",
+    "component": "OpenAccessPolicy"
+  },
+  {
+    "id": "archiving-policy",
+    "title": "Archiving policy",
+    "component": "ArchivingPolicy"
+  },
+  {
+    "id": "repository-policy",
+    "title": "Repository policy",
+    "component": "RepositoryPolicy"
+  },
+  {
+    "id": "journal-metrics",
+    "title": "Journal Metrics",
+    "component": "JournalMetrics"
+  },
+  {
+    "id": "advertising-policy",
+    "title": "Advertising Policy",
+    "component": "AdvertisingPolicy"
+  }
+]
+
+const EPDropdown: React.FC = () => {
+  const [openIndex, setOpenIndex] = useState<string | null>(null);
+
+  const toggle = (id: string) => {
+    setOpenIndex((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <div className="flex flex-col gap-2 mt-20 mb-14 -mx-4">
+      {policy.map((item) => (
+        <div key={item.id} className="rounded-md overflow-hidden">
+          <button
+            onClick={() => toggle(item.id)}
+            className="flex justify-start gap-4 items-center w-full px-5 py-3 text-white font-medium rounded-md bg-gradient-to-b from-[#FF8C42] to-[#995428] hover:from-[#fae0d0] hover:to-[#fae0d0] hover:text-primary-text focus:outline-none transition-all"
+          >
+            <span>{item.title}</span>
+            {openIndex === item.id ? <ChevronUp /> : <ChevronDown />}
+          </button>
+          {openIndex === item.id && (
+            <div className="bg-white text-dark p-4 border border-orange-300 rounded-md mt-2 shadow shadowSprade">
+              {item.component}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EPDropdown;
---
+++ src/ui/endpoints/about/publication-policy/PublicationContent.tsx
@@ -0,0 +1,32 @@
+import Title from '../../../other/Title'
+
+export default function PublicationContent() {
+    return (
+        <div className="space-y-4 pb-4 text-muted-foreground text-base text-[#333333]">
+            <Title>Publication Ethics</Title>
+            <p className="mt-6">
+                <span className="font-semibold">Originality & Plagiarism:</span> Submitted manuscripts must be original and not plagiarized. Proper citations must be given for any borrowed content.
+            </p>
+
+            <p>
+                <span className="font-semibold">Authorship Criteria:</span> All listed authors must have significantly contributed to the research. Ghost, guest, or gift authorship is prohibited.
+            </p>
+
+            <p>
+                <span className="font-semibold">Conflict of Interest:</span> Authors must disclose any financial or personal conflicts that could influence the research.
+            </p>
+
+            <p>
+                <span className="font-semibold">Data Integrity:</span> Fabrication, falsification, or manipulation of data is unethical and will lead to rejection or retraction.
+            </p>
+
+            <p>
+                <span className="font-semibold">Simultaneous Submission:</span> Submitting the sFDRP e manuscript to multiple journals at once is not allowed.
+            </p>
+
+            <p>
+                <span className="font-semibold">Peer Review:</span> The journal likely follows a peer-review process to ensure quality.
+            </p>
+        </div>
+    )
+}
