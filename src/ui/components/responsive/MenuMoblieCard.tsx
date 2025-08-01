f60d159e45dae2bb8d41300a8ecf9a0bf3d8e4a5
tuhinkairi <tuhinkairii@gmail.com>
2025-08-01 13:49:02 +0530

    update the mobile menu

--- src/ui/components/responsive/MenuMoblieCard.tsx
+++ src/ui/components/responsive/MenuMoblieCard.tsx
@@ -1,8 +1,26 @@
 import { X } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { about_menu, auther_menu, conference_menu, thesis_menu } from "../../../data/listingSection";
+import { useAppSelector } from "../../../lib/store/store";
+import { useEffect, useState } from "react";
 
 const MenuMoblieCard = ({ onClose }: { onClose: () => void }) => {
+    const activeIssue = useAppSelector(s => s.archiveSection.indexPage);
+      const [IssueUrl, setIssueUrl] = useState<string>("/current-issue");
+      // update the current issue url
+      useEffect(()=>{
+        setIssueUrl( activeIssue &&
+        Array.isArray(activeIssue) &&
+        activeIssue[0] &&
+        activeIssue[0].volumes &&
+        Array.isArray(activeIssue[0].volumes) &&
+        activeIssue[0].volumes[0] &&
+        activeIssue[0].volumes[0].issue &&
+        Array.isArray(activeIssue[0].volumes[0].issue) &&
+        activeIssue[0].volumes[0].issue[0]
+          ? `/current-issue/paperlist?year=${activeIssue[0].year}&volume=${activeIssue[0].volumes[0].volume}&issue=${activeIssue[0].volumes[0].issue[0]}`
+          : "/current-issue")
+      },[activeIssue])
     return (
         <div className="fixed inset-0 bg-white z-[9999] p-6 overflow-y-auto">
             {/* Close Button */}
@@ -17,7 +35,7 @@ const MenuMoblieCard = ({ onClose }: { onClose: () => void }) => {
                 <NavLink
                     to="/"
                     className={({ isActive }) =>
-                        `block font-medium ${isActive ? "text-primary" : "text-black"
+                        `block font-medium ${isActive ? "text-orange-500" : "text-black"
                         }`
                     }
                     onClick={onClose}
@@ -53,7 +71,7 @@ const MenuMoblieCard = ({ onClose }: { onClose: () => void }) => {
                 </div>
 
                 {/* Single Links */}
-                <NavLink to="/current-issue/paperlist?year=2025&volume=5&issue=3" className="block font-semibold" onClick={onClose}>
+                <NavLink to={IssueUrl} className="block font-semibold" onClick={onClose}>
                     Current Issue
                 </NavLink>
                 <NavLink to="/archives" className="block font-semibold" onClick={onClose}>
