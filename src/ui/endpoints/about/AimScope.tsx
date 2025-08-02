f0f3d8beca235d3c962e5142a6dd3de6737706f0
tuhinkairi <tuhinkairii@gmail.com>
2025-08-02 23:27:35 +0530

    aim&scope update

--- src/ui/endpoints/about/AimScope.tsx
+++ src/ui/endpoints/about/AimScope.tsx
@@ -8,7 +8,7 @@ const AimScope = () => {
       <section>
         <Title2>Aim</Title2>
         <p className=" leading-relaxed text-gray-600">
-          The International Journal Of Scientific Research in Engineering & Technology (IJRTMR) is an international journal dedicated to the latest advancements in engineering and technology. This journal aims to provide a platform for scientists and academicians worldwide to promote, share, and discuss various new issues and developments in different areas of engineering and technologies.
+          The International Journal Of Recent Trends in Multidisciplinary (IJRTMR) is an international journal dedicated to the latest advancements in engineering and technology. This journal aims to provide a platform for scientists and academicians worldwide to promote, share, and discuss various new issues and developments in different areas of engineering and technologies.
         </p>
       </section>
 
@@ -19,7 +19,7 @@ const AimScope = () => {
           The journal's scope is to provide an academic medium and an essential reference for advancing and disseminating research results that support high-level learning, teaching, and research in engineering & technology. We bring together scientists, academicians, engineers, scholars, and students of related fields.
         </p>
         <p className="leading-relaxed text-gray-600">
-          The International Journal of Scientific Research in Engineering and Technology (IJRTMR) is an open-access, peer-reviewed multidisciplinary publication that offers authors a worldwide platform to share their research articles online. IJRTMR adheres to an open-access paradigm that allows for the unrestricted sharing of research papers to the global community via the internet. All articles released under open access can be viewed freely.
+          The International Journal Of Recent Trends in Multidisciplinary (IJRTMR) is an open-access, peer-reviewed multidisciplinary publication that offers authors a worldwide platform to share their research articles online. IJRTMR adheres to an open-access paradigm that allows for the unrestricted sharing of research papers to the global community via the internet. All articles released under open access can be viewed freely.
         </p>
       </section>
     </div>
